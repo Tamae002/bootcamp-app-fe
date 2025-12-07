7dfb91e770f3f8a85ff2b6085721afaedd15ca2a
Raihan Khairul Rochman <rkhairulrochman912@gmail.com>
2025-12-07 15:14:40 +0700

    Update config for allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,28 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [["babel-plugin-react-compiler"]],
+        },
+      }),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
       },
-    }),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve("./src"),
     },
-  },
+    server: {
+      allowedHosts: env.ALLOWED_HOSTS.split(","),
+    },
+  };
 });
