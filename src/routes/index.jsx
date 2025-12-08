8db8d5f5b26a1289717576cd3790df7ccc2d6b0c
Raihan Khairul Rochman <rkhairulrochman912@gmail.com>
2025-12-08 10:04:37 +0700

    Add forgot passwords routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import Login from "@/pages/auth/Login.jsx";
 import AuthProvider from "@/contexts/auth/AuthProvider.jsx";
+import ForgotPassword from "@/pages/ForgotPassword.jsx";
+import PendingApproval from "@/pages/PendingApproval.jsx";
+import ResetPassword from "@/pages/ResetPassword.jsx";
 
 export default function AppRouter() {
   return (
@@ -9,7 +12,9 @@ export default function AppRouter() {
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={/* TODO: Halaman forgot password */ null} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/pending-approval" element={<PendingApproval />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
