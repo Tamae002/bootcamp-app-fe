fe89d6aef876454cdf4be084a67b957a1febf179
Raihan Khairul Rochman <rkhairulrochman912@gmail.com>
2025-12-17 11:26:04 +0700

    Enhance responsiveness

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -2,6 +2,7 @@ import authApi from "@/api/auth.api";
 import LoginImage from "@/assets/images/login_image.png";
 import Logo from "@/assets/images/logo/logotype.png";
 import LogoDark from "@/assets/images/logo/logotype_dark.png";
+import Throbber from "@/components/Throbber";
 import { useAuth } from "@/contexts/auth";
 import { useTheme } from "@/contexts/theme";
 import formDataToJson from "@/lib/formDataToJson";
@@ -55,18 +56,18 @@ export default function Login() {
   return (
     <>
       <title>Login - Geeksfarm</title>
-      <main className="flex min-h-screen min-w-full flex-col-reverse md:flex-row">
+      <main className="flex min-h-svh min-w-full scrollbar-hidden flex-col-reverse md:flex-row">
         <form
           noValidate
           onSubmit={handleLogin}
-          className="md:py-auto flex flex-1 flex-col items-stretch justify-center gap-9 px-6 max-md:-translate-y-24 md:px-28"
+          className="md:py-auto max-md:m-auto md:min-w-140 max-md:max-w-120 flex flex-1 flex-col items-stretch justify-center gap-9 px-6 max-md:-translate-y-24 md:px-28"
         >
           <img
             className="max-md:hidden"
             src={theme == "dark" ? LogoDark : Logo}
           />
 
-          <img src={LoginImage} />
+          <img className="max-h-54 object-contain object-center" src={LoginImage} />
 
           <div>
             {error && <p className="text-red mb-4 text-sm">{error}</p>}
@@ -95,20 +96,19 @@ export default function Login() {
             <a href="/forgot-password">Lupa Password?</a>
           </div>
 
-          <input
-            type="submit"
-            value={loading ? "Memproses..." : "Masuk"}
-            className="button"
-          />
+          <button type="submit" className="button">
+            {loading && <Throbber />}
+            Masuk
+          </button>
         </form>
 
         <aside className="flex-2">
           <div className="bg-primary flex h-full w-full flex-col justify-center gap-4 px-5 pt-20 pb-32 md:px-20 md:py-0">
-            <h1 className="text-6xl text-white md:text-9xl">
-              Bootcamp <br />
+            <h1 className="text-6xl text-white lg:text-7xl xl:text-8xl 2xl:text-9xl">
+              Bootcamp
               Platform
             </h1>
-            <p className="font-light text-white md:text-4xl">
+            <p className="font-light text-pretty text-white md:text-2xl xl:text-3xl 2xl:text-4xl">
               Platform terpadu untuk mengakses kelas, pertemuan, dan tugas
               dengan efisien.
             </p>
--- src/pages/misc/Loading.jsx
+++ src/pages/misc/Loading.jsx
@@ -1,9 +1,9 @@
-import Throbber from "@/components/misc/Throbber";
+import Throbber from "@/components/Throbber";
 
 export default function Loading() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <Throbber />
+    <div className="w-screen h-svh flex justify-center items-center">
+      <Throbber width="64px" height="64px" lineWidth="12px" />
     </div>
   );
 }
